0ff9e1189f20cc33887cb3f741482857330024fc
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-08-12 20:21:29 -0300

    🔧 config: configurações iniciais

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <nav>
+  <nav id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/basket">Carrinho de compras (0)</router-link>
   </nav>
   <router-view/>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 1280px;
+  margin: 80px auto;
 }
 
-nav {
-  padding: 30px;
-}
+#nav {
+  padding: 16px 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
+  background-color: rgb(37, 37, 37);
+  color: white;
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
+  a {
+    color: white;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      color: #007bff;
+    }
+  }
 }
 </style>
---
+++ src/views/ShoppingBasket.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Shopping Basket</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ShoppingBasket'
+}
+</script>
