8cf1c5bd492e1e285c47d6d4e45f4649509dc4bd
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-08-12 21:51:40 -0300

    ✨ feat: armazenando compras no localStorage

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import { mapState } from 'vuex';
 export default {
   created() {
     this.$store.dispatch('loadProducts');
+    this.$store.dispatch('loadBag');
   },
   computed: mapState([
     'productsInBag'
--- src/store/index.ts
+++ src/store/index.ts
@@ -13,11 +13,16 @@ export default createStore<IState>({
     loadProducts(state, products) {
       state.products = products;
     },
+    loadBag(state, products) {
+      state.productsInBag = products;
+    },
     addToBag(state, product) {
       state.productsInBag.push(product);
+      localStorage.setItem('productsInBag', JSON.stringify(state.productsInBag));
     },
     removeFromBag(state, productId: number) {
       state.productsInBag = state.productsInBag.filter((item: { id: number }) => item.id !== productId);
+      localStorage.setItem('productsInBag', JSON.stringify(state.productsInBag));
     }
   },
   actions: {
@@ -26,6 +31,11 @@ export default createStore<IState>({
         .then(res => {
           commit('loadProducts', res.data);
         });
+    },
+    loadBag({ commit }) {
+      if (localStorage.getItem('productsInBag')) {
+        commit('loadBag', JSON.parse(localStorage.getItem('productsInBag') || '[]'));
+      }
     },
     addToBag({ commit }, product) {
       commit('addToBag', product);
